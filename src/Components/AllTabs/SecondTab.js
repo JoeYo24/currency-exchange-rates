5feb70ee292a0633d6af4793b93dbc7848a10119
JoeYo24 <jjsteffen@hotmail.com>
2023-05-21 19:12:56 -0400

    Added Tab Component

---
+++ src/Components/AllTabs/SecondTab.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const SecondTab = () => {
+    return (
+        <div className="SecondTab">
+            <p>Second Tab</p>
+            {/* second tab content will go here */}
+        </div>
+    );
+};
+
+export default SecondTab;
